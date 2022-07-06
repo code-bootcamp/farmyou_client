974694da245bf937c7ded8418a08170dd83198b2
KMS9612 <kimdev9612@gmail.com>
2022-07-06 20:48:58 +0900

    #18-Style 결제페이지 퍼블리싱 1일차(반응형미완)

---
+++ src/components/units/users/payment/payment.styles.ts
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled";
+
+export const DevideLine = styled.div`
+  width: 100%;
+  border-top: 1px solid #bdbdbd;
+  margin: 12px 0px;
+`;
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 50px 0px;
+`;
+export const Header = styled.h1``;
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+`;
+export const Text = styled.div`
+  width: 12%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ComponentWrapper = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Select = styled.select`
+  width: 15%;
+  height: 50px;
+  margin-right: 10px;
+  border: 1px solid #bdbdbd;
+  border-radius: 5px;
+`;
+export const Option = styled.option``;
+
+export const AddressWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 60%;
+  height: 170px;
+`;
+
+export const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
