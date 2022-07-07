2041db0a984455eb830ff0e9c715d3030bd76019
yooyeonhee <devyh0629@gmail.com>
2022-07-07 21:42:37 +0900

    #19-Style 메인퍼블리싱 / 헤더 작업 후 다시 진행

---
+++ src/components/units/main/Main.styles.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Body = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 800px;
+  background-color: yellow;
+`;
