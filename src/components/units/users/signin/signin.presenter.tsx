5892768e6bf0698ea304f8f34b3658d11edfbaf2
WonYongHyeon <quin1392@gmail.com>
2022-07-08 22:41:44 +0900

    #59-Style 회원가입 페이지 퍼블리싱 수정 및 반응형 적용

--- src/components/units/users/signin/signin.presenter.tsx
+++ src/components/units/users/signin/signin.presenter.tsx
@@ -9,69 +9,54 @@ export default function SigninUI() {
         <S.Header>회원가입</S.Header>
         <S.InputWrapper>
           <S.Text>이메일</S.Text>
-          <S.DevideLine></S.DevideLine>
           <InputComponent placeholder="이메일을 입력하세요" />
         </S.InputWrapper>
         <S.InputWrapper>
           <S.Text>이름</S.Text>
-          <S.DevideLine></S.DevideLine>
           <InputComponent placeholder="이름을 입력하세요" />
         </S.InputWrapper>
         <S.InputWrapper>
           <S.Text>비밀번호</S.Text>
-          <S.DevideLine></S.DevideLine>
           <InputComponent placeholder="비밀번호를 입력하세요" />
         </S.InputWrapper>
         <S.InputWrapper>
           <S.Text>비밀번호확인</S.Text>
-          <S.DevideLine></S.DevideLine>
           <InputComponent placeholder="비밀번호를 다시 입력하세요" />
         </S.InputWrapper>
         <S.InputWrapper>
-          <S.Text>휴대전화</S.Text>
-          <S.DevideLine></S.DevideLine>
-          {/* 번호 입력부분 */}
-          <S.PhoneWrapper>
-            <S.Select id="PhoneNumber">
-              <option selected value={"010"}>
-                010
-              </option>
-              <option value={"011"}>011</option>
-              <option value={"016"}>016</option>
-              <option value={"017"}>017</option>
-            </S.Select>
-            -<InputComponent placeholder="전화번호"></InputComponent>
-            <S.BtnWrapper>
-              <S.Text>03:00</S.Text>
-              <ButtonComponent title="인증번호 전송" buttonColor="#F6651E" />
-            </S.BtnWrapper>
-          </S.PhoneWrapper>
-          <S.PhoneWrapper>
-            <InputComponent placeholder="인증번호 입력" />
-            <S.BtnWrapper>
-              <ButtonComponent title="인증하기" buttonColor="#F6651E" />
-            </S.BtnWrapper>
-          </S.PhoneWrapper>
-        </S.InputWrapper>
-        <S.InputWrapper>
           <S.Text>주소</S.Text>
-          <S.DevideLine></S.DevideLine>
           <S.Address>
             <S.ZipcodeWrapper>
               <S.AddressInput>
-                <InputComponent placeholder="우편번호를 입력하세요" />
+                <InputComponent placeholder="00000" />
               </S.AddressInput>
               <S.AddressBtn>
-                <ButtonComponent title="우편번호 검색" buttonColor="#F6651E" />
+                <S.AddressSearchBtn>우편번호 검색</S.AddressSearchBtn>
               </S.AddressBtn>
             </S.ZipcodeWrapper>
-            <S.AddressWrapper>
-              <InputComponent placeholder="주소" />
-              <InputComponent placeholder="상세주소" />
-            </S.AddressWrapper>
+            {/* <S.AddressWrapper> */}
+            <InputComponent placeholder="주소" />
+            <InputComponent placeholder="상세주소" />
+            {/* </S.AddressWrapper> */}
           </S.Address>
         </S.InputWrapper>
-        <S.DevideLine></S.DevideLine>
+        <S.InputWrapper>
+          <S.Text>휴대전화</S.Text>
+          <S.PhoneWrapper>
+            <InputComponent placeholder="" />
+            <S.Text style={{ padding: "0 1vw" }}> - </S.Text>
+            <InputComponent placeholder="" />
+            <S.Text style={{ padding: "0 1vw" }}> - </S.Text>
+            <InputComponent placeholder="" />
+          </S.PhoneWrapper>
+        </S.InputWrapper>
+
+        <S.BtnWrapper>
+          <InputComponent placeholder="인증번호 입력" />
+          <S.Text style={{ justifyContent: "center" }}>03:00</S.Text>
+          <S.AddressSearchBtn>인증번호 전송</S.AddressSearchBtn>
+        </S.BtnWrapper>
+
         <S.SubmitWrapper>
           <ButtonComponent title="취소하기" />
           <ButtonComponent title="가입하기" buttonColor="#F6651E" />
--- src/components/units/users/signin/signin.styles.ts
+++ src/components/units/users/signin/signin.styles.ts
@@ -2,125 +2,139 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
   display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
   margin: 0px auto;
   background-image: url("/bgi/bgi.signup.png");
   background-size: cover;
-  /* @media (max-width: 575px) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  } */
+  padding: 5vw;
+  @media (max-width: 575px) {
+  }
 `;
+
 export const Header = styled.div`
-  font-size: 2vw;
+  font-size: 2rem;
   font-weight: 700;
-  margin-bottom: 30px;
+  margin-bottom: 15px;
 `;
 
 // 폼 랩
 export const FormWrapper = styled.div`
-  width: 60%;
+  min-width: 750px;
+  width: 50%;
   height: 100%;
   border: 1px solid #bdbdbd;
   border-radius: 5px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  padding: 20px;
+  padding: 4vw;
+  margin: 8vw 0;
+  @media (max-width: 750px) {
+    width: 100%;
+    min-width: 0px;
+  }
+  @media (max-width: 575px) {
+    min-width: 0px;
+  }
 `;
 
 export const InputWrapper = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: flex-start;
-  margin-bottom: 10px;
-`;
-export const Text = styled.div`
-  font-size: 1.5vw;
-  font-weight: 700;
-`;
-export const DevideLine = styled.div`
-  border-top: 1px solid #f6651e;
-  width: 100%;
-  padding: 10px 0px;
-`;
-export const PhoneWrapper = styled.div`
-  width: 100%;
-  display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  height: 70px;
-`;
-
-export const BtnWrapper = styled.div`
-  padding: 0px 0px 0px 30px;
-  width: 35%;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
+  justify-content: space-between;
+  /* align-items: center; */
+  text-align: center;
+  padding: 1vw;
   @media (max-width: 575px) {
+    /* min-width: 0px; */
     flex-direction: column;
   }
 `;
 
-export const Select = styled.select`
-  width: 20%;
-  height: 2.5vw;
-  min-height: 35px;
+export const Text = styled.div`
+  font-size: 1.5rem;
+  font-weight: 700;
+  width: 180px;
+  display: flex;
+
+  align-items: center;
   text-align: center;
-  font-size: 16px;
-  border: 1px solid #bdbdbd;
-  border-radius: 5px;
   @media (max-width: 575px) {
-    height: 35px;
-    width: 50%;
+    font-size: 1.2rem;
+    width: 130px;
   }
 `;
-export const Option = styled.option`
-  text-align: center;
+
+export const DivideLine = styled.div`
+  border-top: 1px solid #f6651e;
+  width: 100%;
+  padding: 10px 0px;
+`;
+
+export const PhoneWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  /* height: 70px; */
+`;
+
+export const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1vw;
+
+  /* text-align: center; */
+  @media (max-width: 575px) {
+    /* width: 100%; */
+    /* flex-direction: column; */
+  }
 `;
 
 // Address 랩
 export const Address = styled.div`
   width: 100%;
-  height: 170px;
+  height: 10vw;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  min-height: 140px;
   @media (max-width: 575px) {
-    height: 110px;
+    /* height: 110px; */
   }
 `;
+
 export const ZipcodeWrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 
 export const AddressInput = styled.div`
   width: 50%;
 `;
+
 export const AddressBtn = styled.div`
   width: 100%;
 `;
-export const AddressWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 100px;
-  padding-bottom: 20px;
+
+export const AddressSearchBtn = styled.button`
+  width: 10vw;
+  min-width: 150px;
+  height: 100%;
+  background-color: #d9d9d9;
+  font-size: 1.5rem;
+  border: 0px solid;
+  border-radius: 35px;
   @media (max-width: 575px) {
-    height: 100px;
+    min-width: 100px;
+    font-size: 0.8rem;
   }
 `;
 
@@ -129,4 +143,5 @@ export const SubmitWrapper = styled.div`
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
+  margin-top: 2vw;
 `;
