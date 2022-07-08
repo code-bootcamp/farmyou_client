5b6104827097e2ba97a19091b3b411f124bc7007
yooyeonhee <devyh0629@gmail.com>
2022-07-08 09:30:47 +0900

    #58-Style 메인페이지 퍼블리싱 콘텐츠 추가 / 헤더 디자인 후 재수정

--- src/components/units/main/Main.presenter.tsx
+++ src/components/units/main/Main.presenter.tsx
@@ -2,15 +2,39 @@ import * as S from "./Main.styles";
 export default function MainUI() {
   return (
     <S.Body>
-      <div style={{ height: "100vh" }}>
+      <S.VideoWrapper>
         <S.MainVideo
           muted={true}
           autoPlay={true}
           loop={true}
           src="/bgv/main.mp4"
         />
-      </div>
-      <S.Wrapper></S.Wrapper>
+      </S.VideoWrapper>
+      {/* <S.Title>
+        생산자를 보호하고
+        <br /> 소비자의 먹거리 안전을 보장하는
+        <span style={{ fontWeight: "700" }}>로컬푸드</span> 온라인 마켓
+      </S.Title> */}
+      <S.SubTitle>
+        생산자를 보호하고소비자의 먹거리 안전을 보장하는 <br />
+        <span style={{ fontWeight: "700" }}>로컬푸드</span> 온라인 마켓
+      </S.SubTitle>
+      {/* <S.Title>“행복한 밥상 팜유와 함께하세요”</S.Title> */}
+      <S.NextIcon>
+        <S.Icon src="/icons/arrow.svg" />
+      </S.NextIcon>
+      <S.InfoWrapper>
+        <S.InfoTitle>로컬푸드란?</S.InfoTitle>
+        <S.InfoContents>
+          <span style={{ fontSize: "2vw" }}>로컬푸드</span>는{" "}
+          <b>
+            지역에서 생산한 먹거리를 장거리 이동과 다단계 유통과정을 거치지 않고
+            지역에서 소비하는 새로운 먹거리
+          </b>{" "}
+          <br /> 유통문화로 장거리 운송을 거치지 않은 지역 농산물(흔히 반경 50km
+          이내에서 생산된 농산물)을 지칭합니다.
+        </S.InfoContents>
+      </S.InfoWrapper>
     </S.Body>
   );
 }
--- src/components/units/main/Main.styles.ts
+++ src/components/units/main/Main.styles.ts
@@ -11,9 +11,60 @@ export const MainVideo = styled.video`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  position: relative;
 `;
-export const Wrapper = styled.div`
+export const VideoWrapper = styled.div`
   width: 100%;
-  height: 800px;
-  background-color: yellow;
+  height: 100vh;
+  position: relative;
+`;
+export const SubTitle = styled.div`
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  top: 70vh;
+  left: 0;
+  font-size: 1.6vw;
+  z-index: 3;
+  color: white;
+`;
+export const Title = styled.div`
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  top: 75vh;
+  left: 0;
+  font-size: 2vw;
+  z-index: 3;
+  color: white;
+`;
+export const NextIcon = styled.div`
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  top: 85vh;
+  font-size: 2vw;
+  z-index: 3;
+`;
+export const Icon = styled.img`
+  width: 3vw;
+`;
+
+export const InfoWrapper = styled.div`
+  padding: 10vh;
+  width: 100%;
+  height: 600px;
+`;
+export const InfoTitle = styled.div`
+  width: 100%;
+  text-align: center;
+  color: #f58220;
+  font-size: 8vh;
+  margin-bottom: 2vw;
+`;
+
+export const InfoContents = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 3vh;
 `;
