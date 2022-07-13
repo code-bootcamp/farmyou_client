078eced035acda0a459978775262b3575a095826
yooyeonhee <devyh0629@gmail.com>
2022-07-13 17:24:51 +0900

    #89-Feat 못난이 상품 등록 input-form 연결 / 버튼 이벤트 연결(api)작업 필요

--- src/components/units/bfood/write/BfoodWrite.container.tsx
+++ src/components/units/bfood/write/BfoodWrite.container.tsx
@@ -1,5 +1,38 @@
 import BfoodWriteUI from "./BfoodWrite.presenter";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/router";
+
+const schema = yup.object({
+  name: yup.string().required(),
+  price: yup.string().required(),
+  quantity: yup.string().required(),
+  area: yup.number().required(),
+});
 
 export default function BfoodWrite() {
-  return <BfoodWriteUI />;
+  const router = useRouter();
+  const { register, handleSubmit, setValue, trigger } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const onChangeContents = (value: string) => {
+    setValue("description", value === "<p><br></p>" ? "" : value);
+    trigger("description");
+  };
+
+  const onClickToCancel = () => {
+    router.push("/users/mypage");
+  };
+  // console.log(watch());
+  return (
+    <BfoodWriteUI
+      register={register}
+      handleSubmit={handleSubmit}
+      onChangeContents={onChangeContents}
+      onClickToCancel={onClickToCancel}
+    />
+  );
 }
