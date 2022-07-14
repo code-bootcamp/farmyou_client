0bbbd48fee889fd71bff5b9830d016e9831c45c7
yooyeonhee <devyh0629@gmail.com>
2022-07-14 15:39:10 +0900

    111-Feat 못난리 리스트 페이지 구현(fetch) / 이미지 업로드 구현 후 수정 필요

--- src/components/units/bfood/list/BfoodList.container.tsx
+++ src/components/units/bfood/list/BfoodList.container.tsx
@@ -1,5 +1,8 @@
+import { useQuery } from "@apollo/client";
 import BfoodListUI from "./BfoodList.presenter";
+import { FETCH_UGLY_PRODUCTS } from "./BfoodList.queries";
 
 export default function BfoodList() {
-  return <BfoodListUI></BfoodListUI>;
+  const { data } = useQuery(FETCH_UGLY_PRODUCTS);
+  return <BfoodListUI data={data} />;
 }
---
+++ src/components/units/bfood/list/BfoodList.types.ts
@@ -0,0 +1,3 @@
+export interface IBfoodListUIProps {
+  data: any;
+}
