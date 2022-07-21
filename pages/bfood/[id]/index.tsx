7e310ced08d1f4a08297d94018c77ae23fde6b9e
WonYongHyeon <quin1392@gmail.com>
2022-07-21 11:47:19 +0900

    #184-Fix 상품 상세 페이지 api 변경에 따른 버그 수정 완료

--- pages/bfood/[id]/index.tsx
+++ pages/bfood/[id]/index.tsx
@@ -1,16 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import Detail from "../../../src/components/units/detail/Detail.container";
-import { FETCH_UGLY_PRODUCT } from "../../../src/components/units/detail/Detail.queries";
+import { FETCH_PRODUCT_UGLY } from "../../../src/components/units/detail/Detail.queries";
 
 export default function BfoodDetailPage() {
   const router = useRouter();
 
-  const { data, loading } = useQuery(FETCH_UGLY_PRODUCT, {
+  const { data, loading } = useQuery(FETCH_PRODUCT_UGLY, {
     variables: {
       productId: router.query.id,
     },
   });
 
-  return loading || <Detail data={data.fetchUglyProduct}></Detail>;
+  return loading || <Detail data={data.fetchProductUgly}></Detail>;
 }
